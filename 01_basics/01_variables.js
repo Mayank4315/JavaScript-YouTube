5c1b790e8c2128673fa7d43a08311a4c65f5a6a0
Mayank Kashyap <56481716+Mayank4315@users.noreply.github.com>
2022-12-25 18:32:00 +0000

    added variable folder

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 454631
+let accountEmail = "mayankkashyap@gmail.com"
+var accoountPassword = "12345"
+accountCity = "muzaffarpur"
+
+let accountState;
+
+// accountId = 2 // not allowed in this way
+
+accountEmail = "hc@hc.com"
+accoountPassword = "2112221"
+accountCity = "jaipur"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accoountPassword,accountCity,accountState])
+
+
+/*
+prefe not use var  
+because of issue in  block scope and functional scope
+
+*/
