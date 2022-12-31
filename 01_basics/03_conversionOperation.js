3ec7498cc9c0bce433dfc3d92cfb768df636aa64
Mayank Kashyap <56481716+Mayank4315@users.noreply.github.com>
2022-12-31 19:10:59 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,2 +1,33 @@
-let score = 33
+// let score = "33abc"
+
+// let     score = null
+
+// let score = true
+
+
+
+// console.log(typeof score)
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn  = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber  = String(someNumber)
+console.log(someNumber);
+console.log(typeof stringNumber)
+
+
 
